c2441978fbd6fa3628321158d6d13b1d67886a03
Elaina <elaina.hardebeck@gmail.com>
2022-07-20 20:13:06 -0400

    initial commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpsRedirection(options => options.HttpsPort = 443);
+
+
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+{
+webBuilder.UseStartup<Startup>();
+webBuilder.UseUrls("http://localhost:80", "https://localhost:443");
+});
+
+
+
 
 var app = builder.Build();
 
